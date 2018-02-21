598e049bb6a4cd3a2fb6ae9e6b65fea5de8666c9
lauren defrancesco <lauren209@live.com>
2018-02-21 15:04:22 -0500

    changing the cpp and h files

--- include/functions.h
+++ include/functions.h
@@ -1,57 +1,9 @@
 
-#include <string>
-#include <iostream>
-#include <stack>
 
-class functions
-{
-public:
-  functions();
-  ~functions();
-
-
- bool balancedSymbols(std::string input){
-  int inputSize = input.length();
-  char* char_array = new char[inputSize];
-  // symbolArray = new char[inputSize];
-
-
-  for(int i = 0; i< inputSize; i++){
-    char_array[i] = input.at(i);
-  }
-
-  for(int j=0; j<inputSize; j++){
-    if(char_array[j] == "[" || char_array[j] == "{" || char_array[j] == "("){
-      symbolStack.push(char_array[i]);
-    }
-
-    else{
-
-      if(char_array[j] == "]" && symbolStack.peak() == "["){
-        symbolStack.pop();
-      }
-      else if(char_array[j] == "}" && symbolStack.peak() == "{"){
-        symbolStack.pop();
-      }
-      else if(char_array[j] == ")" && symbolStack.peak() == "("){
-        symbolStack.pop();
-      }
-    }
-
-  }
-
-
- }
-
-
+bool balancedSymbols(std::string input);
 
 
  std::string iToP(std::string input);
 
-private:
-  // char* symbolArray;
-  std::stack<string> symbolStack;
-  bool isBalanced;
-  std::string postFix;
 
-};
+
--- src/functions.cpp
+++ src/functions.cpp
@@ -2,8 +2,43 @@
 #include "../include/catch.h"
 
 
-int main(){
 
 
+#include <string>
+#include <iostream>
+#include <stack>
 
-}
+
+ bool balancedSymbols(std::string input){
+  int inputSize = input.length();
+  char* char_array = new char[inputSize];
+  // symbolArray = new char[inputSize];
+
+
+  for(int i = 0; i< inputSize; i++){
+    char_array[i] = input.at(i);
+  }
+
+  for(int j=0; j<inputSize; j++){
+    if(char_array[j] == "[" || char_array[j] == "{" || char_array[j] == "("){
+      symbolStack.push(char_array[i]);
+    }
+
+    else{
+
+      if(char_array[j] == "]" && symbolStack.peak() == "["){
+        symbolStack.pop();
+      }
+      else if(char_array[j] == "}" && symbolStack.peak() == "{"){
+        symbolStack.pop();
+      }
+      else if(char_array[j] == ")" && symbolStack.peak() == "("){
+        symbolStack.pop();
+      }
+    }
+
+  }
+
+
+ }
+
